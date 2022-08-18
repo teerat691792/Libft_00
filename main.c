b6783e5cc04559e192340e7f2e4f8f22c674e4fc
Teerat691792 <teerat691792@gmail.com>
2022-08-18 16:08:56 +0700

    3rd

--- main.c
+++ main.c
@@ -1,19 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkulket <tkulket@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/15 15:10:51 by tkulket           #+#    #+#             */
-/*   Updated: 2022/08/18 15:52:20 by tkulket          ###   ########.fr       */
+/*   Created: 2022/08/15 18:02:22 by tkulket           #+#    #+#             */
+/*   Updated: 2022/08/18 15:47:55 by tkulket          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalpha(int c)
+#include <stdio.h>
+#include <ctype.h>
+
+int	ft_isalpha(int c);
+
+int	main(void)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1024);
-	else
-		return (0);
+	int	c;
+
+	c = 'M';
+	printf("var = %c\n", c);
+	printf("var = %d\n", c);
+	printf("ft_built = %d\n", ft_isalpha(c));
+	printf("alpha  = %d\n", isalpha(c));
+	printf("lower = %d\n", islower(c));
+	printf("upper = %d\n", isupper(c));
+	printf("digit = %d\n", isdigit(c));
+	return (0);
 }
